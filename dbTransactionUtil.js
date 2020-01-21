89c87a5c01abe47600482d6c567a59e92610638a
Danilo Herrera <danilo@agileninja.io>
2020-01-21 17:26:51 -0500

    Clean up dbTransactionUtil

--- dbTransactionUtil.js
+++ dbTransactionUtil.js
@@ -1,25 +1,19 @@
 import createClient from './databaseClient.js'
 
-const util = {
+export default {
     runTransaction: async (func) => {
-        return await transaction(func)
+        const client = createClient()
+        try {
+            await client.connect()
+            await client.query("BEGIN")
+            const value = await func(client)
+            await client.query("COMMIT")
+            return value
+        } catch (ex) {
+            console.log(ex)
+            await client.query("ROLLBACK")
+        } finally {
+            client.end()
+        }
     }
 }
-
-async function transaction(func) {
-    const client = createClient()
-    try {
-        await client.connect()
-        await client.query("BEGIN")
-        const value = await func(client)
-        await client.query("COMMIT")
-        return value
-    } catch (ex) {
-        console.log(ex)
-        await client.query("ROLLBACK")
-    } finally {
-        client.end()
-    }
-}
-
-export default util
