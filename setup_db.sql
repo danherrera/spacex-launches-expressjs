5eb99610c536594eec1c34fe795b0ca8eb9c333d
Danilo Herrera <danilo@agileninja.io>
2020-01-21 18:44:06 -0500

    Add dev setup script

---
+++ setup_db.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS launch;
+DROP DATABASE IF EXISTS spacex_launches;
+CREATE DATABASE spacex_launches;
+\c spacex_launches;
+CREATE TABLE launch (
+    id SERIAL PRIMARY KEY,
+    rocket_name VARCHAR(50) NOT NULL,
+    rocket_type VARCHAR(50) NOT NULL,
+    launch_date TIMESTAMP NOT NULL,
+    details TEXT NOT NULL,
+    article_link TEXT NOT NULL,
+    reddit_launch_link TEXT NOT NULL
+);
